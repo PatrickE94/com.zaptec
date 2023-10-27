a2cf5b87f9ba43b2bb7da1a20b273e8bc716d61e
Patrick Wadström <patrick@weblab.se>
2023-10-27 22:04:07 +0200

    Add a PoC Home driver for testing in beta

---
+++ drivers/home/driver.ts
@@ -0,0 +1,121 @@
+import Homey from 'homey';
+import { ChargerOperationMode, chargerOperationModeStr, ZaptecApi } from '../../lib/zaptec';
+import type { ProCharger } from './device';
+
+interface InstallationCurrentControlArgs {
+  current1: number;
+  current2: number;
+  current3: number;
+  device: ProCharger;
+}
+
+class HomeDriver extends Homey.Driver {
+  /**
+   * onInit is called when the driver is initialized.
+   */
+  async onInit() {
+    this.log('HomeDriver has been initialized');
+    this.registerFlows();
+  }
+
+  protected registerFlows() {
+    this.log('HomeDriver is registering flows');
+
+    this.homey.flow
+      .getActionCard('installation_current_control')
+      .registerRunListener(
+        async ({
+          current1,
+          current2,
+          current3,
+          device,
+        }: InstallationCurrentControlArgs) => {
+          this.log(
+            `[${device.getName()}] Action 'installation_current_control' triggered`,
+          );
+          this.log(
+            `[${device.getName()}] - current: '${current1}/${current2}/${current3}' amps`,
+          );
+
+          return device.setInstallationAvailableCurrent(
+            current1,
+            current2,
+            current3,
+          );
+        },
+      );
+
+    this.homey.flow
+      .getConditionCard('pro_is_charging')
+      .registerRunListener(async ({ device }) =>
+        device.getCapabilityValue('charging_button'),
+      );
+
+    this.homey.flow
+      .getConditionCard('pro_is_connected')
+      .registerRunListener(
+        async ({ device }) =>
+          !!device.getCapabilityValue('alarm_generic.car_connected'),
+      );
+
+    this.homey.flow
+      .getConditionCard('pro_charging_is_finished')
+      .registerRunListener(
+        async ({ device }) =>
+          device.getCapabilityValue('charge_mode') ===
+          chargerOperationModeStr(ChargerOperationMode.Connected_Finishing),
+      );
+
+    this.homey.flow
+      .getActionCard('pro_start_charging')
+      .registerRunListener(async ({ device }) => device.startCharging());
+
+    this.homey.flow
+      .getActionCard('pro_stop_charging')
+      .registerRunListener(async ({ device }) => device.stopCharging());
+  }
+
+  async onPair(session: Homey.Driver.PairSession) {
+    const api = new ZaptecApi();
+    let username = '';
+    let password = '';
+
+    session.setHandler(
+      'login',
+      async (data: { username: string; password: string }) => {
+        username = data.username;
+        password = data.password;
+
+        try {
+          await api.authenticate(username, password);
+          return true;
+        } catch (_error) {
+          return false;
+        }
+      },
+    );
+
+    session.setHandler('list_devices', async () => {
+      const chargers = await api.getChargers({
+        DeviceType: 3 /* home */,
+        //InstallationType: 0 /* pro */,
+      });
+
+      return (
+        chargers.Data?.map((charger) => ({
+          name: `${charger.Name}`,
+          data: {
+            id: charger.Id,
+            installationId: charger.InstallationId,
+          },
+          settings: {
+            username,
+            password,
+          },
+        })) || []
+      );
+    });
+  }
+}
+
+module.exports = HomeDriver;
