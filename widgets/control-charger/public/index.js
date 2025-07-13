75a17ed865b52a3c9406fffca446254ba95ef420
philipostli <55027973+philipostli@users.noreply.github.com>
2025-07-13 14:28:47 +0200

    Removed material-web

--- widgets/control-charger/public/index.js
+++
@@ -1,5 +0,0 @@
-// index.js
-import '@material/web/iconbutton/filled-icon-button.js';
-import '@material/web/icon/icon.js';
-import '@material/web/fab/fab.js';
-import '@material/web/fab/branded-fab.js';
